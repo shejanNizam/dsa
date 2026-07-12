1866d3571ce4ccb09d974a951c48520715e7385e
nizam <nizam.shejan11@gmail.com>
2026-07-12 09:51:32 +0600

    array practice

--- arrays/bubble-sort/bubble.js
+++ arrays/bubble-sort/bubble.js
@@ -1,9 +1,17 @@
 //  Bubble Sort is an algorithm that sorts an array from the lowest value to the highest value.
 
-const myArray = [64, 34, 25, 12, 22, 11, 90, 5];
+const myArray = [64, 34, 25, 12, 22, 11, 90, 5]; // length = 8
 
 const n = myArray.length;
 
 for (let i = 0; i < n - 1; i++) {
-  console.log(i);
+  for (j = 0; j < n - i - 1; j++) {
+    if (myArray[j] > myArray[j + 1]) {
+      if (myArray[j] > myArray[j + 1]) {
+        [myArray[j], myArray[j + 1]] = [myArray[j + 1], myArray[j]];
+      }
+    }
+  }
 }
+
+console.log("Sorted Array: ", myArray);
---
+++ js/practice.js
@@ -0,0 +1,24 @@
+// loop
+// const cars = ["BMW", "Volvo", "Saab", "Ford"];
+
+// let i = 0;
+// let text = "";
+
+// for (; cars[i]; ) {
+//   text += cars[i];
+//   i++;
+// }
+
+const fruits = ["Apple", "Banana", "Cherry"];
+let x = "";
+myfavorite: {
+  x = fruits[0];
+  break myfavorite;
+  x = fruits[1];
+  x = fruits[2];
+}
+
+// array
+const points = new Array(1, 5, 4);
+
+console.log(points);
